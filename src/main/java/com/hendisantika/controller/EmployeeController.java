cff35dabc598e50bdebbfa170fb45a8662971f96
Hendi Santika <hendisantika@gmail.com>
2022-01-19 06:53:09 +0700

    #25 - Add EmployeeController

---
+++ src/main/java/com/hendisantika/controller/EmployeeController.java
@@ -0,0 +1,30 @@
+package com.hendisantika.controller;
+
+import com.hendisantika.service.AuthenticationService;
+import com.hendisantika.service.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-swagger-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/01/22
+ * Time: 06.52
+ */
+@RestController
+public class EmployeeController {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+}

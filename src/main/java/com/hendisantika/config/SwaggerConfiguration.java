a597d84f09de7e5e33bbde20e8900ceb92782079
Hendi Santika <hendisantika@gmail.com>
2022-01-19 06:06:02 +0700

    #8 - Add defaultAuth in SwaggerConfiguration

--- src/main/java/com/hendisantika/config/SwaggerConfiguration.java
+++ src/main/java/com/hendisantika/config/SwaggerConfiguration.java
@@ -6,13 +6,16 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -59,5 +62,13 @@ public class SwaggerConfiguration {
         return SecurityContext.builder()
                 .securityReferences(defaultAuth())
                 .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
     }
 }

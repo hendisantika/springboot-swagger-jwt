dcceb8421030d5d39cac22ec853f4f2352e8c91d
Hendi Santika <hendisantika@gmail.com>
2022-01-19 06:01:11 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/SpringbootSwaggerJwtApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSwaggerJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSwaggerJwtApplication.class, args);
+    }
+
+}

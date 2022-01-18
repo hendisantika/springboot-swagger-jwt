859e63afbf3a7203667c53d925fe2b2e91a8c3ff
Hendi Santika <hendisantika@gmail.com>
2022-01-19 06:04:35 +0700

    #5 - Add Docket API in SwaggerConfiguration

--- src/main/java/com/hendisantika/config/SwaggerConfiguration.java
+++ src/main/java/com/hendisantika/config/SwaggerConfiguration.java
@@ -1,9 +1,15 @@
 package com.hendisantika.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 /**
@@ -29,5 +35,17 @@ public class SwaggerConfiguration {
                 "License of API",
                 "API license URL",
                 Collections.emptyList());
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }

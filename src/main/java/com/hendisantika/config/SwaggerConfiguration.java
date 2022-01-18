2874723c50baa0c97b9efa7d4d558f5919dde0f9
Hendi Santika <hendisantika@gmail.com>
2022-01-19 06:02:30 +0700

    #3 - Add SwaggerConfiguration

---
+++ src/main/java/com/hendisantika/config/SwaggerConfiguration.java
@@ -0,0 +1,18 @@
+package com.hendisantika.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-swagger-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/01/22
+ * Time: 06.02
+ */
+@Configuration
+public class SwaggerConfiguration {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+}

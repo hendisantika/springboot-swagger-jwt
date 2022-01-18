3a6abbc0ebe3d3374c3e6a85205b70fcd9d2e9ec
Hendi Santika <hendisantika@gmail.com>
2022-01-19 06:44:21 +0700

    #12 - Add SwaggerConfiguration

--- src/main/java/com/hendisantika/config/SwaggerConfiguration.java
+++ src/main/java/com/hendisantika/config/SwaggerConfiguration.java
@@ -1,21 +1,19 @@
 package com.hendisantika.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
 
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,48 +25,111 @@ import java.util.List;
  * Time: 06.02
  */
 @Configuration
+@OpenAPIDefinition(info = @Info(title = "My API", version = "v1"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class SwaggerConfiguration {
 
     public static final String AUTHORIZATION_HEADER = "Authorization";
 
-    private ApiInfo apiInfo() {
-        return new ApiInfo("My REST API",
-                "Some custom description of API.",
-                "1.0",
-                "Terms of service",
-                new Contact("Hendi Santika", "https://linktr.ee/hendisantika", "hendisantika@yahoo.co.id"),
-                "License of API",
-                "API license URL",
-                Collections.emptyList());
-    }
+    public static final String SECURITY_SCHEME_NAME = "Bearer oAuth Token";
 
+    //    private ApiInfo apiInfo() {
+//        return new ApiInfo("My REST API",
+//                "Some custom description of API.",
+//                "1.0",
+//                "Terms of service",
+//                new Contact("Hendi Santika", "https://linktr.ee/hendisantika", "hendisantika@yahoo.co.id"),
+//                "License of API",
+//                "API license URL",
+//                Collections.emptyList());
+//    }
+//
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(apiInfo())
+//                .securityContexts(Arrays.asList(securityContext()))
+//                .securitySchemes(Arrays.asList(apiKey()))
+//                .select()
+//                .apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//
+//    private ApiKey apiKey() {
+//        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+//    }
+//
+//    private SecurityContext securityContext() {
+//        return SecurityContext.builder()
+//                .securityReferences(defaultAuth())
+//                .build();
+//    }
+//
+//    List<SecurityReference> defaultAuth() {
+//        AuthorizationScope authorizationScope
+//                = new AuthorizationScope("global", "accessEverything");
+//        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+//        authorizationScopes[0] = authorizationScope;
+//        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+//    }
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .securityContexts(Arrays.asList(securityContext()))
-                .securitySchemes(Arrays.asList(apiKey()))
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI openApiConfiguration(
+            @Value("${openapi.title}") final String title,
+            @Value("${openapi.version}") final String version,
+            @Value("${openapi.description}") final String description
+    ) {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                        new io.swagger.v3.oas.models.security.SecurityScheme()
+                                                .name(SECURITY_SCHEME_NAME)
+                                                .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title(title)
+                        .version(version)
+                        .description(description)
+                        .termsOfService("Terms of service")
+                        .license(getLicense())
+                        .contact(getContact())
+                );
     }
 
-    private ApiKey apiKey() {
-        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    /**
+     * Contact details for the developer(s)
+     *
+     * @return
+     */
+    private Contact getContact() {
+        Contact contact = new Contact();
+        contact.setEmail("hendisantika@yahoo.co.id");
+        contact.setName("Hendi Santika");
+        contact.setUrl("https://linktr.ee/hendisantika");
+        contact.setExtensions(Collections.emptyMap());
+        return contact;
     }
 
-    private SecurityContext securityContext() {
-        return SecurityContext.builder()
-                .securityReferences(defaultAuth())
-                .build();
-    }
-
-    List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope
-                = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    /**
+     * License creation
+     *
+     * @return
+     */
+    private License getLicense() {
+        License license = new License();
+        license.setName("Apache License, Version 2.0");
+        license.setUrl("http://www.apache.org/licenses/LICENSE-2.0");
+        license.setExtensions(Collections.emptyMap());
+        return license;
     }
 }
